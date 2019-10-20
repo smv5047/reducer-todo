a802dbbe0acc4660afe28610f4450a44c2ae66a9
smv5047 <smv5047@gmail.com>
2019-10-20 15:52:33 -0400

    updated reducer file

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
+
+
+
+export function reducer (state, action) {
+    
+    return {
+        ...state
+    }
+}
